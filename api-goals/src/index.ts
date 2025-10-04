46bd45208401188a202bcdfd87bc5b9dbba658bf
Aldito8 <alditositompul198@gmail.com>
2025-10-04 19:31:46 +0700

    small change

--- api-goals/src/index.ts
+++ api-goals/src/index.ts
@@ -13,10 +13,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: process.env.CORS_ALLOWED_ORIGINS,
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: process.env.CORS_ALLOWED_ORIGINS,
+//     credentials: true,
+// }));
 
 app.post('/register', register)
 app.post('/login', login)
@@ -32,7 +32,12 @@ app.get('/users/:id', getUserDetail)
 app.put('/users/:id', updateUser)
 app.delete('/users/:id', deleteUser)
 
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
-export default app;
+app.use(cors({
+    origin: "https://kepengen-app.vercel.app",
+    credentials: true
+}));
+app.options('*', cors());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
