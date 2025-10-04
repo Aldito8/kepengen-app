8e3090ba808c941698d0c149190b31d2b7174495
Aldito8 <alditositompul198@gmail.com>
2025-10-04 19:41:02 +0700

    small change

--- api-goals/src/index.ts
+++ api-goals/src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { upload } from './utils/multer';
 import { deleteUser, getUser, getUserDetail, updateUser } from './controllers/admin';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express()
 const port = process.env.PORT;
@@ -13,10 +15,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors({
-//     origin: process.env.CORS_ALLOWED_ORIGINS,
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: process.env.CORS_ALLOWED_ORIGINS,
+    credentials: true,
+}));
 
 app.post('/register', register)
 app.post('/login', login)
@@ -31,12 +33,7 @@ app.get('/users', getUser)
 app.get('/users/:id', getUserDetail)
 app.put('/users/:id', updateUser)
 app.delete('/users/:id', deleteUser)
-
-app.use(cors({
-    origin: "https://kepengen-app.vercel.app",
-    credentials: true
-}));
-app.options('*', cors());
+console.log(process.env.CORS_ALLOWED_ORIGINS)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
