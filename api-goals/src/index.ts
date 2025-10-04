f98fc2401ea154108e0e8a24ea700ea14789ef6e
Aldito8 <alditositompul198@gmail.com>
2025-10-04 15:36:09 +0700

    cors adjustment

--- api-goals/src/index.ts
+++ api-goals/src/index.ts
@@ -14,7 +14,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ALLOWED_ORIGINS,
     credentials: true,
 }));
 
